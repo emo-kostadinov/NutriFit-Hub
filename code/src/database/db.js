1ca47960e78cd34028dde87534b01425886c7391
emo-kostadinov <117582924+emo-kostadinov@users.noreply.github.com>
2024-01-21 17:17:28 +0200

    Add files via upload

--- code/src/database/db.js
+++ code/src/database/db.js
@@ -1,1 +1,14 @@
+import mysql from 'mysql2';
 
+const pool = mysql.createPool({
+  host: 'mysql',
+  user: 'root',
+  password: 'Emotae06',
+  database: 'nutrifit_hub_db', 
+  port: 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+}).promise();
+
+export default pool;
